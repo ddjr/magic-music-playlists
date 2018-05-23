dc2f68762b34794c228fa38cf836566e22d831c2
Your Name <ddalyjr@gmail.com>
2018-05-23 12:44:37 -0500

    added support for playlist filter on client

--- src/App.js
+++ src/App.js
@@ -71,7 +71,7 @@ class Filter extends Component {
     return (
       <div style={defaultStyle}>
         <img />
-        <input type="text"/>
+        <input type="text" onChange={event => this.props.onTextChange(event.target.value)}/>
         Filter
       </div>
     );
@@ -97,7 +97,8 @@ class App extends Component {
   constructor() {
     super()
     this.state = {
-      serverData: {}
+      serverData: {},
+      filterString: ''
     }
   }
   componentDidMount() {
@@ -119,8 +120,11 @@ class App extends Component {
           </h1>
           <PlaylistCounter playlists={this.state.serverData.user.playlists} />
           <HoursCounter playlists={this.state.serverData.user.playlists} />
-        <Filter />
-        {this.state.serverData.user.playlists.map(playlist =>
+        <Filter onTextChange={text => this.setState({filterString: text})}/>
+        {this.state.serverData.user.playlists.filter(playlist =>
+          playlist.name.toLowerCase().includes(
+            this.state.filterString.toLowerCase())
+        ).map(playlist =>
           <Playlist playlist={playlist}/>
         )}
       </div> : <h1 style={defaultStyle}>Loading...</h1>
